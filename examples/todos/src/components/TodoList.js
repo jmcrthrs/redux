ca51c8c1202027936fb76c2b0a15624ddf476759
Jamie <jamie.carruthers@live.com>
2017-10-18 11:43:33 +0200

    Update TodoList.js

--- examples/todos/src/components/TodoList.js
+++ examples/todos/src/components/TodoList.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import Todo from './Todo'
 
-const TodoList = ({ todos, onTodoClick }) => (
+// import our action for use in this component
+import { toggleTodo } from '../actions'
+
+const TodoList = ({ todos, dispatch }) => (
   <ul>
     {todos.map(todo =>
       <Todo
         key={todo.id}
         {...todo}
-        onClick={() => onTodoClick(todo.id)}
+        // dispatch our action here
+        // It's easier to see what effects this onClick method will trigger
+        // I also have intellisence and go to function capabilities in my IDE. 
+        onClick={() => dispatch(toggleTodo(todo.id))} 
       />
     )}
   </ul>
@@ -20,7 +27,8 @@ TodoList.propTypes = {
     completed: PropTypes.bool.isRequired,
     text: PropTypes.string.isRequired
   }).isRequired).isRequired,
-  onTodoClick: PropTypes.func.isRequired
+  dispatch: PropTypes.func.isRequired // use this to dispatch actions
 }
 
-export default TodoList
+// connect to store so we have a dispatch function
+export default connect()(TodoList);
